c097bce836628c783872d79c7451a407dcc69535
Rakibul Hasan <rakibulhasan745101@gmail.com>
2023-07-16 12:46:53 +0600

    reviews application

---
+++ index.js
@@ -0,0 +1,62 @@
+const reviews = [
+    {
+        "id": 1,
+        "name": "susan smith",
+        "job": "web developer",
+        "img": "https://www.course-api.com/images/people/person-1.jpeg",
+        "text": "I'm baby meggings twee health goth +1. Bicycle rights tumeric chartreuse before they sold out chambray pop-up. Shaman humblebrag pickled coloring book salvia hoodie, cold-pressed four dollar toast everyday carry"
+    },
+    {
+        "id": 2,
+        "name": "anna johnson",
+        "job": "web designer",
+        "img": "https://www.course-api.com/images/people/person-2.jpeg",
+        "text": "Helvetica artisan kinfolk thundercats lumbersexual blue bottle. Disrupt glossier gastropub deep v vice franzen hell of brooklyn twee enamel pin fashion axe.photo booth jean shorts artisan narwhal."
+    },
+    {
+        "id": 3,
+        "name": "peter jones",
+        "job": "intern",
+        "img": "https://www.course-api.com/images/people/person-4.jpeg",
+        "text": "Sriracha literally flexitarian irony, vape marfa unicorn. Glossier tattooed 8-bit, fixie waistcoat offal activated charcoal slow-carb marfa hell of pabst raclette post-ironic jianbing swag."
+    },
+    {
+        "id": 4,
+        "name": "bill anderson",
+        "job": "the boss",
+        "img": "https://www.course-api.com/images/people/person-3.jpeg",
+        "text": "Edison bulb put a bird on it humblebrag, marfa pok pok heirloom fashion axe cray stumptown venmo actually seitan. VHS farm-to-table schlitz, edison bulb pop-up 3 wolf moon tote bag street art shabby chic. "
+    }
+];
+
+console.log(reviews)
+
+const imgTag = document.querySelector('img');
+const nameTag = document.querySelector('h3');
+const jobTag = document.querySelector('h4');
+const detailsTag = document.querySelector('p');
+const prevBtn = document.querySelector('.prev-btn');
+const nextBtn = document.querySelector('.next-btn')
+let number = 0
+
+
+window.addEventListener('DOMContentLoaded', () => {
+    setReview();
+})
+
+prevBtn.addEventListener('click', () => {
+    number === 0 ? number = reviews.length - 1 : --number;
+    setReview();
+})
+
+nextBtn.addEventListener('click', () => {
+    number === reviews.length - 1 ? number = 0 : ++number;
+    setReview();
+})
+
+const setReview = () => {
+    imgTag.src = reviews[number].img;
+    nameTag.innerText = reviews[number].name;
+    jobTag.textContent = reviews[number].job;
+    detailsTag.textContent = reviews[number].text;
+}
